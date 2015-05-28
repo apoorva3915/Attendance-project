65612795f3f58908ca66dd50b1b4802512e26bb7
Hatim Kanchwala <hatimkanchwala@gmail.com>
2015-05-28 20:40:18 +0530

    Removed call to scan() from auth() and enrol()

--- r305-fingerprint/r305-fingerprint.c
+++ r305-fingerprint/r305-fingerprint.c
@@ -28,20 +28,12 @@ uint1 scan() {
 uint1 auth(uint2 *uID) {
     uint1 i;
 
-    i = scan();
-    if (i != CMD_SUCCESS)
-        return i;
-
     return srchLib(0x01, uID);
 }
 
 uint1 enrol() {
     uint1 i;
     uint2 uID;
-
-    i = scan();
-    if (i != CMD_SUCCESS)
-        return i;
 
     switch (i = srchLib(0x01, &uID)) {
     case MATCH_SUCCESS:
